1fe56cf728b67f1d772ca254bec5b876d93ddf11
dackJaniel <daniel.hilmer@outlook.de>
2023-12-20 13:47:05 +0100

    Update package.json and add test.html and posts.js files

--- app/js/posts.js
+++ app/js/posts.js
@@ -1,24 +1,40 @@
-"use strikt";
-
-// const axios = require("axios");
-
-// const getPosts = async () => {
-//     let posts;
-//     try {
-//         const response = axios.get("localhost:5000/api/posts");
-//         console.log(response);
-//     } catch (error) {
-//         console.log("Something went wrong: ", error);
-//     }
-// };
-
 const getPosts = async () => {
-    let posts;
-    try {
-        const response = await fetch("http://localhost:5000/api/posts");
-        posts = await response.json();
-        console.log(posts);
-    } catch (error) {}
+  try {
+    const response = await fetch("https://jsonplaceholder.org/posts");
+    return await response.json();
+  } catch (error) {
+    console.log("Something went wrong: ", error);
+  }
 };
 
-getPosts();
+const getUsers = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.org/users");
+    return await response.json();
+  } catch (error) {
+    console.log("Something went wrong: ", error);
+  }
+};
+
+const print = async () => {
+  const postDiv = document.getElementById("posts");
+  const posts = await getPosts();
+  const users = await getUsers();
+
+  posts.forEach((post) => {
+    postDiv.innerHTML += `
+        <div class="card">
+            <div class="card-body">
+            <small class="text-muted">${users[post.userId].firstname} ${
+      users[post.userId].lastname
+    }</small>
+                <img src="${post.thumbnail}" alt="${post.title}" />
+                <h5 class="card-title">${post.title}</h5>
+                <p class="card-text">${post.body}</p>
+            </div>
+        </div>
+        `;
+  });
+};
+
+print();
